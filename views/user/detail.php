befca2dde0b789fcf375961609d0d96e7bb26ac0
MingTih <caroline.vlhuynh@gmail.com>
2022-02-03 21:42:14 +0100

    1 - ajout fichiers UserController, User et detail.php

---
+++ views/user/detail.php
@@ -0,0 +1,22 @@
+<?php  include VIEWS.'inc/header.php'; ?>
+    <div>
+        <h1>Mes informations</h1>
+
+
+        <ul>
+            
+
+
+        </ul>
+
+
+
+    </div>
+
+
+
+
+
+
+
+<?php  include VIEWS.'inc/footer.php'; ?>
