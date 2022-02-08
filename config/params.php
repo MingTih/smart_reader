0edabe63e424a63a816a08352cf93b1173dcac69
MingTih <caroline.vlhuynh@gmail.com>
2022-02-09 00:25:44 +0100

    récup version distant sur pc maison

--- config/params.php
+++ config/params.php
@@ -50,14 +50,8 @@ $routes = [
     // Table user : Utilisateurs--------------------------------------------------------
     '/monCompte'            => ['UserController', 'compteDetail'], //OK
     '/modifCompte'          => ['UserController', 'replaceUser'],
-<<<<<<< HEAD
     '/connexion'            => ['UserController', 'connexion'],
     '/inscription'          => ['UserController', 'replaceUser'],
-=======
-    '/connexion'            => ['UserController', 'connexion'],//OK
-    '/inscription'          => ['UserController', 'inscription'],
-    //Déconnexion OK
->>>>>>> daadca0254963ae992fc1bd314541469f39131c0
 
     // Table et API : Offres, demandes et échanges
     '/mesOffres'            => ['ProductController', 'offersList'],
