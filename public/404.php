cd7f2c1cf243353a6954849513b2ec15e3a32d45
Salima <salima@yahoo.fr>
2022-02-14 14:51:06 +0100

    ajout page 404

--- public/404.php
+++ public/404.php
@@ -1,5 +1,33 @@
 <?php
 
-if(!session_status()) session_start();
+if (!session_status()) session_start();
 $_SESSION['messages']['danger'][] = "La page demandée n'existe pas.";
-AppController::index();
+//AppController::index();// html page 404
+?>
+<?php
+include VIEWS . "inc/header.php";
+
+
+?>
+<style>
+  header,
+  footer {
+    display: none;
+  }
+</style>
+<div class="text">
+  <div>ERREUR</div>
+  <h1>404</h1>
+  <hr>
+  <div>Page Non trouvée</div>
+</div>
+
+<div class="mascotte404">
+
+  <img src="<?= ASSET ?>images/mascotte1.png" alt="" class="src">
+</div>
+
+
+<?php
+
+include VIEWS . "inc/footer.php";
