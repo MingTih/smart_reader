c5f2ab2492225685b1daf335207e77c73238a616
pamela <p.tavares93270@gmail.com>
2022-02-15 10:38:40 +0100

    changed params

--- config/params.php
+++ config/params.php
@@ -5,11 +5,11 @@
  */
 const CONFIG = [
     'db' => [
-        'DB_HOST' => 'localhost:8889',
+        'DB_HOST' => 'localhost',
         'DB_PORT' => '3306',
         'DB_NAME' => 'smart-reader',
         'DB_USER' => 'root',
-        'DB_PSWD' => 'root',
+        'DB_PSWD' => '',
     ],
     'app' => [
         'name' => 'smart_reader',
